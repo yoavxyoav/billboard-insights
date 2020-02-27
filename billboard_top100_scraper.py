8ef6353fa2075f1e2017f538dc581708097fdea2
= <=>
2020-02-28 01:03:54 +0200

    completing most of data_dict (before images)

--- billboard_top100_scraper.py
+++ billboard_top100_scraper.py
@@ -22,4 +22,9 @@ item = soup.find('li', class_='chart-list__element')
 item_data = {}
 
 # collecting each item's data from left to right, as it appears on the site
-item_data.rank = item.find('span', class_='chart-element__rank')
+item_data['rank'] = item.find('span', class_='chart-element__rank__number').text
+item_data['song'] = item.find('span', class_='chart-element__information__song').text
+item_data['artist'] = item.find('span', class_='chart-element__information__artist').text
+item_data['last_pos'] = item.find('span', class_='text--last').text
+item_data['peak'] = item.find('span', class_='text--peak').text
+item_data['duration'] = item.find('span', class_='text--week').text
