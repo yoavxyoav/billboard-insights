bd169be6114f429f31c6b119f290a943bb0eb691
= <=>
2020-04-12 20:37:40 +0300

    Yossi's argparse stuff in main(). Currently commented out until further check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from billboard_scraper import Scraper
 from query_handler import Query
+import argparse
 
 def except_on_error(func):
     """
@@ -17,50 +18,32 @@ def except_on_error(func):
     return wrapper
 
 
+def get_args():
+    """define parser and return user input"""
+    parser = argparse.ArgumentParser(description='Web Scraper : please specify data and/or action ')
+    parser.add_argument('command', type=str, help='desired function',
+                        choices=('all', 'single', 'range'))
+    command = parser.parse_known_args()[0].command
+    if command in ('single', 'range'):
+        parser.add_argument('start_date', type=str, help='first number')
+        if command == 'range':
+            parser.add_argument('end_date', type=str, help='second number')
+    args = parser.parse_args()
+    return command, list(args.__dict__.values())[1:]
+
 @except_on_error
 def main():
-    ###
-    ### The following commented text will be left for the process of the initial development in order to quickly
-    ### test and debug various functions of the parser (instead of rewritten every time).
-    ### they will be removed from the final product.
-    ###
-
-    # # scraping the most recent page (which is the default week for the main url)
-    # soup = get_page_soup(BASE_URL, PARSER)
-
-    # # for testing: getting the first item of the most recent week
-    # some_test_item = get_first_item(soup)
-    # print(some_test_item)
-
-    # week = get_week(soup)
-    # print(week, '\n')
-
-    # # scraping the entire first week
-    # weekly_items = get_weekly_chart(soup)
-    # for item in weekly_items:
-    #     print(item)
-
-    # the real deal: scraping all time, from most recent week to the beginning
+    # action, args = get_args()
 
     query = Query()
     scraper = Scraper(auto_most_recent=True, query=query)
 
+    # action_dict = {'all': scraper.get_all_time, 'single': scraper.get_specific_week, 'range': scraper.get_time_range}
+    # action_dict[action](*args)
 
-    # 
-    # scraper.get_time_range()
-    # all_weeks = scraper.get_all_time()
-    # scraper.get_update_from_time()
-    chart = scraper.get_specific_week('2010-09-14')
-    print(chart.chart)
-    print(chart.week)
-    #
-    # sql_inserter.insert(all_weeks)
-    #
-    # some_week = sql_object.give_me_last_week()
-    #
-    #
-    # scraper = Scraper(333, 333, SQL_inserter)
-    #
+    # scraper.get_specific_week('2010-09-14')
+
+    scraper.get_all_time()
 
 if __name__ == '__main__':
     main()
